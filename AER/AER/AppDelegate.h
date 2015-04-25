f0f2b91bfbbc66206dbf2c168fe91f7bfa4e6e39
Peter Foti <peterfoti@me.com>
2015-04-25 01:24:44 -0400

    Adds base project to repo

---
+++ AER/AER/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AER
+//
+//  Created by Peter Foti on 4/25/15.
+//  Copyright (c) 2015 Peter Foti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AER/AER/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AER
+//
+//  Created by Peter Foti on 4/25/15.
+//  Copyright (c) 2015 Peter Foti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
