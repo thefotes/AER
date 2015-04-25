24e2dce62f7eb041172881c0e207ea01e0579ea8
Peter Foti <peterfoti@me.com>
2015-04-25 01:33:30 -0400

    Removes nonsense

--- AER/AER/ViewController.h
+++ AER/AER/ViewController.h
@@ -1,10 +1,8 @@
-//
 //  ViewController.h
 //  AER
 //
 //  Created by Peter Foti on 4/25/15.
 //  Copyright (c) 2015 Peter Foti. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- AER/AER/ViewController.m
+++ AER/AER/ViewController.m
@@ -1,10 +1,8 @@
-//
 //  ViewController.m
 //  AER
 //
 //  Created by Peter Foti on 4/25/15.
 //  Copyright (c) 2015 Peter Foti. All rights reserved.
-//
 
 #import "ViewController.h"
 
@@ -13,15 +11,5 @@
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
