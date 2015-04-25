dcaaf6321352e1fdb72122024aa2b5f088353060
Peter Foti <peterfoti@me.com>
2015-04-25 11:26:01 -0400

    Adds string constants

---
+++ AER/AER/StringConstants.h
@@ -0,0 +1,15 @@
+//  StringConstants.h
+//  AER
+//
+//  Created by Peter Foti on 4/25/15.
+//  Copyright (c) 2015 Peter Foti. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface StringConstants : NSObject
+
+FOUNDATION_EXPORT NSString * const kFromNumber;
+FOUNDATION_EXPORT NSString * const kAccountSid;
+FOUNDATION_EXPORT NSString * const kAuthToken;
+
+@end
