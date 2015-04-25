ff6338a121a083303fd3093e7166e386308dfcf4
Peter Foti <peterfoti@me.com>
2015-04-25 13:14:16 -0400

    Embeds into tab bar controller and adds contact vc to project

---
+++ AER/AER/ContactsViewController.m
@@ -0,0 +1,13 @@
+//  ContactsViewController.m
+//
+//  Created by Peter Foti on 4/25/15.
+
+#import "ContactsViewController.h"
+
+@interface ContactsViewController ()
+
+@end
+
+@implementation ContactsViewController
+
+@end
