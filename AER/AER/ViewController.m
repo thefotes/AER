36018663708f9bf0146b3961c9eccfba9a5fae22
Peter Foti <peterfoti@me.com>
2015-04-25 10:10:03 -0400

    Adds stubbed out ibaction to test network call

--- AER/AER/ViewController.m
+++ AER/AER/ViewController.m
@@ -5,11 +5,21 @@
 //  Copyright (c) 2015 Peter Foti. All rights reserved.
 
 #import "ViewController.h"
+#import "TwilioCommunicator.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+- (IBAction)buttonPressed:(id)sender
+{
+    [[TwilioCommunicator sharedTwilioCommunicator] sendMessage:@""
+                                                      toNumber:@""
+                                                withCompletion:^(id obj) {
+                                                } andFailure:^(NSError *error) {
+                                                }];
+}
 
 @end
