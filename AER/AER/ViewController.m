8c09235e9cc9611ed4552097fdbd29e6902779e1
Peter Foti <peterfoti@me.com>
2015-04-25 13:05:43 -0400

    Adds code for fetching phone numbers from the phone book

--- AER/AER/ViewController.m
+++ AER/AER/ViewController.m
@@ -6,8 +6,9 @@
 
 #import "ViewController.h"
 #import "TwilioCommunicator.h"
+@import AddressBookUI;
 
-@interface ViewController ()
+@interface ViewController () <ABPeoplePickerNavigationControllerDelegate>
 
 @end
 
@@ -15,11 +16,31 @@
 
 - (IBAction)buttonPressed:(id)sender
 {
-    [[TwilioCommunicator sharedTwilioCommunicator] sendMessage:@"Hello Brian, how are you doing."
-                                                      toNumber:@"3152515028"
-                                                withCompletion:^(id obj) {
-                                                } andFailure:^(NSError *error) {
-                                                }];
+    ABPeoplePickerNavigationController *navController = [[ABPeoplePickerNavigationController alloc] init];
+    navController.peoplePickerDelegate = self;
+    [self presentViewController:navController animated:YES completion:nil];
+}
+
+- (void)peoplePickerNavigationController:(ABPeoplePickerNavigationController*)peoplePicker didSelectPerson:(ABRecordRef)person
+{
+    NSMutableArray *mobilePhones = [NSMutableArray arrayWithCapacity:0];
+    
+    ABMultiValueRef phones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+    NSArray *allPhoneNumbers = (NSArray *)CFBridgingRelease(ABMultiValueCopyArrayOfAllValues(phones));
+    
+    for (NSUInteger i=0; i < [allPhoneNumbers count]; i++) {
+        if ([(NSString *)CFBridgingRelease(ABMultiValueCopyLabelAtIndex(phones, (long)i)) isEqualToString:(NSString *)kABPersonPhoneMobileLabel]) {
+            [mobilePhones addObject:CFBridgingRelease(ABMultiValueCopyValueAtIndex(phones, (long)i))];
+        }
+        if ([(NSString *)CFBridgingRelease(ABMultiValueCopyLabelAtIndex(phones, (long)i)) isEqualToString:(NSString *)kABPersonPhoneIPhoneLabel]) {
+            [mobilePhones addObject:CFBridgingRelease(ABMultiValueCopyValueAtIndex(phones, (long)i))];
+        }
+    }
+    
+    CFRelease(phones);
+    
+    NSLog(@"%@", [mobilePhones firstObject]);
+    
 }
 
 @end
