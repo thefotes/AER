be07be998c2365020a0222924f5b07ac208a3d23
Peter Foti <peterfoti@me.com>
2015-04-25 13:17:19 -0400

    Mocks out table view on contacts vc

--- AER/AER/ContactsViewController.m
+++ AER/AER/ContactsViewController.m
@@ -4,10 +4,35 @@
 
 #import "ContactsViewController.h"
 
-@interface ContactsViewController ()
+@interface ContactsViewController () <UITableViewDelegate, UITableViewDataSource>
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray *mockData;
 @end
 
 @implementation ContactsViewController
+
+- (NSArray *)mockData
+{
+    return _mockData = _mockData ?: @[@"Peter", @"Brian", @"Oren"];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = self.mockData[(NSUInteger)indexPath.row];
+    
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return (NSInteger)self.mockData.count;
+}
 
 @end
