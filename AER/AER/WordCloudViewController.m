82771740cbdf408ec00f2d19d19bc55742e10a4a
Brian M. Palma <brian@brianmpalma.com>
2015-04-25 22:48:22 -0400

    More things

--- AER/AER/WordCloudViewController.m
+++ AER/AER/WordCloudViewController.m
@@ -7,10 +7,9 @@
 #import "WordCloudViewController.h"
 #import "WordCloudCollectionViewCell.h"
 
-@interface WordCloudViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+@interface WordCloudViewController ()
 
-@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
-@property (copy, nonatomic) NSArray *painPoints;
+@property (nonatomic, strong) IBOutletCollection(UIButton) NSArray *buttons;
 
 @end
 
@@ -19,37 +18,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.collectionView registerNib:[UINib nibWithNibName:NSStringFromClass([WordCloudCollectionViewCell class]) bundle:nil] forCellWithReuseIdentifier:NSStringFromClass([WordCloudCollectionViewCell class])];
-    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
-    flowLayout.itemSize = CGSizeMake(100, 80);
-    flowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
-    self.collectionView.collectionViewLayout = flowLayout;
-    self.collectionView.allowsMultipleSelection = YES;
-}
-
-- (NSArray *)painPoints
-{
-    return _painPoints = _painPoints ?: @[@"Fitness", @"Health", @"Mental", @"Physical"];
-}
-
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    WordCloudCollectionViewCell *cell = (WordCloudCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:NSStringFromClass([WordCloudCollectionViewCell class]) forIndexPath:indexPath];
     
-    [cell configureWithString:self.painPoints[(NSUInteger)indexPath.row]];
-    
-    return cell;
-}
-
-- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    WordCloudCollectionViewCell *cell = (WordCloudCollectionViewCell *)[collectionView cellForItemAtIndexPath:indexPath];
-    cell.selected = !cell.selected;
-}
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    return [self.painPoints count];
+    for (UIButton *button in self.buttons) {
+        button.layer.borderColor = button.tintColor.CGColor;
+        button.layer.borderWidth = 1.0;
+        button.layer.cornerRadius = 5.0;
+    }
 }
 
 @end
